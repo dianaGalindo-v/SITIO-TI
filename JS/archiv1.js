14acb90f4b1be1e8ae6fb7aed7a0b9bb020dd1a5
Diana Galindo Vergara <240360@utxicotepec.edu.mx>
2026-01-08 14:29:30 -0600

    crear funciones y estilizado

--- JS/archiv1.js
+++ JS/archiv1.js
@@ -1,3 +1,31 @@
 function saludar(){
     alert("Bienvenido a la Academia de T.I")
 }
+
+function suma(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 + valor2;
+    alert("Resultado de la suma de" +valor1+" y "+valor2+" es: "+resultado);
+}
+
+function resta(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 - valor2;
+    alert("Resultado de la resta de" +valor1+" y "+valor2+" es: "+resultado);
+}
+
+function multiplicacion(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 * valor2;
+    alert("Resultado de la multiplicacion de" +valor1+" y "+valor2+" es: "+resultado);
+}
+
+function division(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 / valor2;
+    alert("Resultado de la division de" +valor1+" y "+valor2+" es: "+resultado);
+}
