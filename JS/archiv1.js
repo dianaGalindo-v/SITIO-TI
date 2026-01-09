0a32dcef30d33810a52541340c5150770282b701
Diana Galindo Vergara <240360@utxicotepec.edu.mx>
2026-01-09 13:23:56 -0600

    prueba, se modificaron unas mayusculas

--- JS/archiv1.js
+++ JS/archiv1.js
@@ -6,26 +6,26 @@ function suma(){
     var valor1 = 24;
     var valor2 = 13;
     var resultado = valor1 + valor2;
-    alert("Resultado de la suma de" +valor1+" y "+valor2+" es: "+resultado);
+    alert("Resultado de la Suma de" +valor1+" y "+valor2+" es: "+resultado);
 }
 
 function resta(){
     var valor1 = 24;
     var valor2 = 13;
     var resultado = valor1 - valor2;
-    alert("Resultado de la resta de" +valor1+" y "+valor2+" es: "+resultado);
+    alert("Resultado de la Resta de" +valor1+" y "+valor2+" es: "+resultado);
 }
 
 function multiplicacion(){
     var valor1 = 24;
     var valor2 = 13;
     var resultado = valor1 * valor2;
-    alert("Resultado de la multiplicacion de" +valor1+" y "+valor2+" es: "+resultado);
+    alert("Resultado de la Multiplicacion de" +valor1+" y "+valor2+" es: "+resultado);
 }
 
 function division(){
     var valor1 = 24;
     var valor2 = 13;
     var resultado = valor1 / valor2;
-    alert("Resultado de la division de" +valor1+" y "+valor2+" es: "+resultado);
+    alert("Resultado de la Division de" +valor1+" y "+valor2+" es: "+resultado);
 }
